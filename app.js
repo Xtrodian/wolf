17e8b8b4322e5108e406cb07b0ae575c6af43a0a
Xtrodian <fernandh0pv94@hotmail.com>
2017-10-10 14:02:15 -0500

    Ya existe la logica

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-console.log('Hola mundo','Fuck yeah!');
+// console.log('Hola mundo','Fuck yeah!');
 
-var nombre ="Fernando";
-var nick= "Xtrodian";
+// var nombre ="Fernando";
+// var nick= "Xtrodian";
 
-console.log(nombre, 'es',nick);
+// console.log(nombre, 'es',nick);
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+
